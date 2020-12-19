b7c7ca5f51c09a6dfda66506665aa71a2b052c21
186526 <i@186526.xyz>
2020-12-20 00:08:53 +0800

    Add avatar

---
+++ source/about/index.md
@@ -0,0 +1,8 @@
+---
+title: 关于我
+date: 2020-12-19 23:04:55
+---
+<div align="center">
+    <a href="https://github.com/186526"><img src='https://github-readme-stats.vercel.app/api?username=186526&hide_border=true' alt='github stats' /></a>
+    <h3>一个爱写bug的开发者</h3>
+</div>
--- themes/suka/source/js/blog-186.js
+++ themes/suka/source/js/blog-186.js
@@ -2,35 +2,35 @@ console.log('\n %c 186526\'s Blog | © 186526 \n', 'color: #fff; background: #44
 window.addEventListener("load", () => {
     whenAvailable("Pjax", () => {
         const pjax = new Pjax({
-            elements:"a:not([target=_blank])",
+            elements: "a:not([target=_blank])",
             selectors: [
                 "title",
                 "meta[name=description]",
                 ".pjax",
             ],
-            cacheBust:!1
+            cacheBust: !1
         });
     });
-    document.addEventListener("pjax:send",send);
-    document.addEventListener("pjax:success",suc);
+    document.addEventListener("pjax:send", send);
+    document.addEventListener("pjax:success", suc);
 });
 window.addEventListener('load', () => {
     if ("serviceWorker" in navigator) { navigator.serviceWorker.register("/sw.js", { scope: "/" }).then((function (e) { })).catch((function (e) { })); let e = !1; navigator.serviceWorker.addEventListener("controllerchange", () => { e || (e = !0, alert("正在更新Service Worker……"), window.location.reload()) }) }
 });
 let send = () => {
-    whenAvailable("NProgress",()=>{
+    whenAvailable("NProgress", () => {
         NProgress.inc();
     });
 };
 let suc = () => {
-    whenAvailable("NProgress",()=>{
+    whenAvailable("NProgress", () => {
         NProgress.done();
     });
-    whenAvailable("LazyLoad",()=>{
+    whenAvailable("LazyLoad", () => {
         new LazyLoad();
     });
-    if(valinestatus){
-        whenAvailable("Valine",()=>{
+    if (valinestatus) {
+        whenAvailable("Valine", () => {
             valinedo();
         });
     }
