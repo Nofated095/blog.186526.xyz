d1336bd247d2c21046aebecb96dbb48caf541ff9
186526 <i@186526.xyz>
2020-12-25 21:47:11 +0800

    Add lazyload

--- themes/suka/source/js/blog-186.js
+++ themes/suka/source/js/blog-186.js
@@ -15,20 +15,44 @@ window.addEventListener("load", () => {
     document.addEventListener("pjax:success", suc);
 });
 window.addEventListener('load', () => {
-    if ("serviceWorker" in navigator) { navigator.serviceWorker.register("/sw.js", { scope: "/" }).then((function (e) { })).catch((function (e) { })); let e = !1; navigator.serviceWorker.addEventListener("controllerchange", () => { e || (e = !0, alert("正在更新Service Worker……"), window.location.reload()) }) }
-    if (/HeadlessChrome/.test(window.navigator.userAgent)) {return;}
-    if(!window.dataLayer) {
-        let d=document,a = d.createElement("script");
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./sw.js', {
+                scope: '/',
+            })
+            .then(function (registration) {
+                console.log(
+                    'ServiceWorker registration successful with scope: ',
+                    registration.scope,
+                );
+            })
+            .catch(function (err) {
+                console.warn('ServiceWorker registration failed: ', err);
+            });
+        let refreshing = false;
+        navigator.serviceWorker.addEventListener('controllerchange', () => {
+            if (refreshing) {
+                return;
+            }
+            refreshing = true;
+            let d = document.querySelector("title");
+            d.innerText = "Refresh 2 update Service Worker - " + d.innerText;
+            console.log('Service Worker 等待更新……');
+        });
+    }
+    if (/HeadlessChrome/.test(window.navigator.userAgent)) { return; }
+    if (!window.dataLayer) {
+        let d = document, a = d.createElement("script");
         a.async = true;
-        a.src="https://www.googletagmanager.com/gtag/js?id=G-ENYRL7T64N";
+        a.src = "https://www.googletagmanager.com/gtag/js?id=G-ENYRL7T64N";
         d.head.appendChild(a);
     }
-    {
-        let d=document,url="/css/fonts.min.css";
+    if (navigator.connection.rtt <= 300 & navigator.connection.downlink >= 1) {
+        let d = document, url = "/css/fonts.min.css";
         d.head.innerHTML += `<link rel="preload" href="${url}" as="style" onload="this.onload=null;this.rel='stylesheet'"><noscript><link rel="stylesheet" href="${url}"></noscript>`;
     }
     window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
+    function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
     gtag('config', 'G-ENYRL7T64N');
 });
@@ -48,7 +72,7 @@ let suc = () => {
         valinedo();
     }
     window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
+    function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
     gtag('config', 'G-ENYRL7T64N');
 };
