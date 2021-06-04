c716a982269e5f524491fcc0b497923d98f31edb
186526 <i@186526.xyz>
2021-06-05 00:55:05 +0800

    Update

--- themes/suka/source/sw.js
+++ themes/suka/source/sw.js
@@ -9,7 +9,7 @@ const { CacheFirst, NetworkFirst, NetworkOnly } = strategies;
 const { ExpirationPlugin } = expiration;
 const { CacheableResponsePlugin } = cacheableResponse;
 
-const cacheSuffixVersion = '-210604a',
+const cacheSuffixVersion = '-210605b',
     // precacheCacheName = core.cacheNames.precache,
     // runtimeCacheName = core.cacheNames.runtime,
     maxEntries = 100;
